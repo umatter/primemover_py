c0ac1fca3ecdcbee782416764a23e5c6efb0629c
Johannes Ladwig <johannesl@me.com>
2022-01-06 18:03:19 +0100

    very broke dags

--- src/worker/__init__.py
+++ src/worker/__init__.py
@@ -1,3 +1,3 @@
-from . import Notify, DataCopy, Agent, CleanUp, api_wrapper, Behavior, ConfigureProfile, Crawler, Info, Jobs, Profile, Proxy, Tasks, TimeHandler, Utilities, s3_parser, s3_wrapper
+from . import DataCopy, Agent, CleanUp, api_wrapper, Behavior, ConfigureProfile, Crawler, Info, Jobs, Profile, Proxy, Tasks, TimeHandler, Utilities, s3_parser, s3_wrapper
 
-__all__ = [Notify, DataCopy, Agent, CleanUp, api_wrapper, Behavior, ConfigureProfile, Crawler, Info, Jobs, Profile, Proxy, Tasks, TimeHandler, Utilities, s3_parser, s3_wrapper]
+__all__ = [DataCopy, Agent, CleanUp, api_wrapper, Behavior, ConfigureProfile, Crawler, Info, Jobs, Profile, Proxy, Tasks, TimeHandler, Utilities, s3_parser, s3_wrapper]
