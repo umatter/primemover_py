bf477c55b9974fd434b3559b37d4abcb6f9ef27b
Johannes Ladwig <johannesl@me.com>
2022-07-22 14:50:55 +0200

    Added makefile.sh to generate missing resouces folders

---
+++ makefile.sh
@@ -0,0 +1,7 @@
+# Generate missing resource folders
+
+mkdir -p resources/cleaned_data
+mkdir -p resources/crawlers
+mkdir -p resources/input_data
+mkdir -p resources/raw_data
+mkdir -p resources/updates
